674708f1b43862ff793696ee13ff67783da378e6
JeanMaxSKrebs <jeanmaxskrebs@gmail.com>
2023-08-16 14:41:25 -0300

    first commit

---
+++ src/main/java/br/edu/ifsul/cstsi/projeto_tads/ProjetoTadsApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsul.cstsi.projeto_tads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTadsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoTadsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/ifsul/cstsi/projeto_tads/ProjetoTadsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsul.cstsi.projeto_tads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoTadsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
